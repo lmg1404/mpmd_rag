bb2a02f1d6c23b650d954e71860198c27e6decbc
lmg1404 <luismg0203@gmail.com>
2024-07-30 18:48:40 -0400

    fixed sensitive information

--- dags/data_engineering/chunking.py
+++ dags/data_engineering/chunking.py
@@ -3,8 +3,30 @@
 """
 from typing import List, Dict
 
+# TODO
 def character_chunking(youtube_data: List[Dict[str, str]]):
+    """ Gets the uploaded videos key from the channel
+    
+    Parameters
+    ----------
+
+    
+    Returns
+    -------
+
+    """
     pass
 
+# TODO
 def word_chunking():
+    """ Gets the uploaded videos key from the channel
+    
+    Parameters
+    ----------
+
+    
+    Returns
+    -------
+
+    """
     pass
