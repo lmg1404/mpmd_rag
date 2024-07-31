8afe6b2273cc0112d9e948bf0bdcafca928ac0f6
lmg1404 <luismg0203@gmail.com>
2024-07-30 22:55:34 -0400

    database initialization

--- dags/data_engineering/upload.py
+++ dags/data_engineering/upload.py
@@ -1,5 +1,5 @@
 """
-    DAG operator to upload data onto a Qdrant server
+    DAG operators to upload data onto a Qdrant server
 """
 
 from dotenv import load_dotenv
@@ -10,14 +10,27 @@ load_dotenv()
 QDRANT_API_KEY = os.getenv('QDRANT_API_KEY')
 QDRANT_URL = os.getenv('QDRANT_URL')
 
-qdrant_client = qdrant_client.QdrantClient(
-    url=QDRANT_URL, 
-    api_key=QDRANT_API_KEY,
+conn = qdrant_client.QdrantClient(
+    url = QDRANT_URL, 
+    api_key = QDRANT_API_KEY,
 )
 
 def get_embedding_model(model: str):
+    embedding_model = None
+    tokenizer = None 
+    vector_size = None
+    return embedding_model, tokenizer, vector_size
+
+def create_collection():
+    collections = conn.get_collections()
+    if not collections:
+        conn.create_collection(
+            collection_name="moreplatesmoredates"
+            vectors_config = qdrant_client.models.VectorParams()
+        )
+
+def vectorize(transcript, model):
     pass
 
-
-def vectorize():
+def upload_to_qdrant():
     pass
