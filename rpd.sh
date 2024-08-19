5a93562d157129343513927e35260577431df8db
lmg1404 <luismg0203@gmail.com>
2024-08-18 22:59:15 -0400

    Bash docstring

--- rpd.sh
+++ rpd.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This file resolves found dependencies when installing q-drant client
+# and installing requirements onto a Linux machine. By downgrading and appending
+# this file effectively replaces the command 'pip freeze > requirements.txt'. 
+
 input1=requirements.txt
 version="1.60.0"
 append="; platform_system==\"Windows\""
